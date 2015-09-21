13dea0f55a67132b3b9a2454d83ca128bd6753e2
Justin Scott <jrs@users.noreply.github.com>
2015-09-21 14:30:49 -0700

    Update helloword.vb
    Added comments to my file

--- helloworld.vb
+++ helloworld.vb
@@ -1,9 +1,15 @@
-Module Module1
-
+Module Module1 
+ 
     Sub Main()
-        Console.WriteLine("Hello World!")
-        Console.ReadLine()
-
+ 
+        Console.WriteLine(“Hello World”) 
+            
+        ‘Console is Class with a method WriteLine() “Hello World” is an Argument for the Method WriteLine()
+        
+        Console.ReadLine() ‘Console is Class with a method ReadLine()
+ 
+        ‘Console.ReadLine() : this line of code will be commented out
+ 
     End Sub
-
+ 
 End Module
