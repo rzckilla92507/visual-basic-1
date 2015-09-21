b66ea01a5b270ce7775a1b0c8c60ffe3928c1ce1
Justin Scott <jrs@users.noreply.github.com>
2015-09-21 14:32:20 -0700

    Create declaringVariables.vb

---
+++ declaringVariables.vb
@@ -0,0 +1,38 @@
+Module Module1 
+ 
+    Sub Main()
+ 
+        Dim x As Integer
+        Dim y As Integer
+ 
+        x = 7
+        y = x + 3
+ 
+        Console.WriteLine(y)
+ 
+        Console.ReadLine()
+ 
+        Dim myFirstName As String
+        
+        myFirstName = “Bob”
+ 
+        Dim myFirstName As String = “Bob”
+ 
+        Console.WriteLine(myFirstName)
+ 
+        Dim x As Integer = 7
+        Dim y As String = “Bob”
+ 
+        Dim myFirstTry As String = x & y
+        Console.WriteLine(myFirstTry)
+ 
+        Dim mySecondTry As Integer = x + y
+        Console.WriteLine(mySecondTry)
+ 
+        Console.ReadLine()
+ 
+        
+ 
+    End Sub
+ 
+End Module
