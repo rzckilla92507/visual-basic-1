a6be815213de4e6b2201e4670c349d0c60315568
Justin Scott <jrs@users.noreply.github.com>
2015-09-23 14:05:20 -0700

    Update expressions.vb

--- expressions.vb
+++ expressions.vb
@@ -1,3 +1,56 @@
 Module Module1
 
+    Sub Main()
+
+        Dim x = 0
+        Dim y = 0
+        Dim a = 0
+        Dim b = 0
+
+        'Assignment Operator
+        x = 3
+
+        'Addition Operator
+        x = 3 + 4
+
+        'Subtraction Operator
+        x = 4 - 3
+
+        'Multipulcation Operator
+        x = 10 * 5
+
+        'Division Operator
+        x = 10 / 5
+
+        'Equality Operator
+        If x = y Then
+            'Stuff goes here
+        End If
+
+        'Greater than or equal to Operator
+        If x >= y Then
+            'Stuff goes here
+        End If
+
+        'Less than or equal to Operator
+        If x <= y Then
+            'stuff goes here
+        End If
+
+        'Condition OR Operator
+        If x > y Or a > b Then
+            'stuff goes here
+        End If
+
+        'Condition AND Operator
+        If x > y And a > b Then
+            'stuff goes here
+        End If
+
+        'Member access and Method invocation
+        Console.WriteLine("Hi!")
+
+
+    End Sub
+
 End Module
