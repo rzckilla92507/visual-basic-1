4f149b2514e9ae964b619e18a1af1ef4b0e56f9f
Justin Scott <jrs@users.noreply.github.com>
2015-09-25 14:46:38 -0700

    Update simpleClasses.vb

--- simpleClasses.vb
+++ simpleClasses.vb
@@ -5,6 +5,10 @@ Module Module1
         'Dim myNewCar As Car = New 
         Dim myNewCar As New Car
 
+        'Dim myOtherCar As New Car("BMW", 2005, 30000)
+
+        CarLot.addToInventory(myNewCar)
+
         myNewCar.Make = "Oldsmobile"
         myNewCar.Model = "Cutlas Supreme"
         myNewCar.Year = 1986
@@ -19,18 +23,34 @@ Module Module1
 
         'Console.WriteLine("{0} - {1} - {2}", myNewCar.Make, myNewCar.Model, myNewCar.Year.toString())
 
-        Console.WriteLine(determineMarketValue(myNewCar))
+        'Console.WriteLine(determineMarketValue(myNewCar))
 
+        Console.WriteLine("Before: " & myNewCar.Make)
+        doByValue(myNewCar)
+        Console.WriteLine("After by Value: " & myNewCar.Make)
+
+        doByReference(myNewCar)
+        Console.WriteLine("After by Reference: " & myNewCar.Make)
         Console.ReadLine()
 
     End Sub
 
-    Function determineMarketValue(ByVal myCar As Car) As Double
+    'Function determineMarketValue(ByVal myCar As Car) As Double
 
-        'Super secret formula
+    '    'Super secret formula
 
-        Return 100.0
-    End Function
+    '    Return 100.0
+    'End Function
+
+    Sub doByValue(ByVal myCar As Car)
+        myCar = New Car
+        myCar.Make = "BMW"
+    End Sub
+
+    Sub doByReference(ByRef myCar As Car)
+        myCar = New Car
+        myCar.Make = "BMW"
+    End Sub
 
 End Module
 
@@ -40,6 +60,38 @@ Public Class Car
     Public Model As String
     Public Year As Integer
     Public Color As String
+    Public Amount As Double
+    Public Surcharge As Double
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(ByVal _make As String, ByVal _year As Integer, ByVal _amount As Double)
+        Me.Make = _make
+        Me.Year = _year
+        Me.Amount = _amount
+        Me.Surcharge = _amount * 1.025
+    End Sub
+
+    Public Sub myMethod(ByVal myParameter As String)
+
+    End Sub
+
+    Public Sub myMethod(ByVal myParameter As String, ByVal myPerameter As String)
+
+    End Sub
+
+    Public Sub myMethod(ByVal myParameter As Integer)
+
+    End Sub
 
 
+End Class
+
+Public Class CarLot
+
+    Public Shared Sub addToInventory(ByVal newCar As Car)
+        'body of the method goes here.
+    End Sub
 End Class
