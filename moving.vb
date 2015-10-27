b88cf37e055cf537ba54b3cd4206399424c43756
Justin Scott <jrs@users.noreply.github.com>
2015-10-27 14:48:29 -0700

    Update moving.vb

--- moving.vb
+++ moving.vb
@@ -1,11 +1,37 @@
 Dim R, L, U, D As Boolean
     Dim score As Integer
 
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        R = True
+        L = False
+        U = True
+        D = False
+
+        Me.BackColor = Color.Crimson
+
+        lblBall1.Width = 15
+        lblBall1.Height = 15
+        lblBall1.Text = ""
+        lblBall1.BackColor = Color.HotPink
+
+        lblBall2.Width = 75
+        lblBall2.Height = 75
+        lblBall2.Text = ""
+        lblBall2.BackColor = Color.Teal
+
+        lblBall3.Width = 250
+        lblBall3.Height = 250
+        lblBall3.Text = ""
+        lblBall3.BackColor = Color.Snow
+
+    End Sub
+
     Private Sub tmrMoveBall1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrMoveBall1.Tick
 
         If R = True Then
             lblBall1.Left = lblBall1.Left + 10
-            If lblBall1.Left > frmMoving.ActiveForm.Width Then
+            If lblBall1.Left > Form1.ActiveForm.Width Then
                 R = False
                 L = True
             End If
@@ -29,44 +55,17 @@ Dim R, L, U, D As Boolean
 
         If D = True Then
             lblBall1.Top = lblBall1.Top + 10
-            If lblBall1.Top > frmMoving.ActiveForm.Height Then
+            If lblBall1.Top > Form1.ActiveForm.Height Then
                 D = False
                 U = True
             End If
         End If
 
     End Sub
-    Private Sub frmMoving_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-        R = True
-        L = False
-        U = True
-        D = False
-
-        Me.BackColor = Color.Black
-
-        lblBall1.Width = 5
-        lblBall1.Height = 5
-        lblBall1.Text = ""
-        lblBall1.BackColor = Color.Honeydew
-
-        lblBall2.Width = 200
-        lblBall2.Height = 200
-        lblBall2.Text = ""
-        lblBall2.BackColor = Color.Teal
-
-        lblBall3.Width = 40
-        lblBall3.Height = 40
-        lblBall3.Text = ""
-        lblBall3.BackColor = Color.Snow
-
-
-
-    End Sub
 
     Private Sub lblBall2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblBall2.Click
-        lblBall2.Left = CInt(Rnd() * frmMoving.ActiveForm.Width)
-        lblBall2.Top = CInt(Rnd() * frmMoving.ActiveForm.Height)
+        lblBall2.Left = CInt(Rnd() * Form1.ActiveForm.Width)
+        lblBall2.Top = CInt(Rnd() * Form1.ActiveForm.Height)
 
     End Sub
 
@@ -87,6 +86,6 @@ Dim R, L, U, D As Boolean
     End Sub
 
     Private Sub tmrMoveBall3_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrMoveBall3.Tick
-        lblBall3.Left = CInt(Rnd() * frmMoving.ActiveForm.Width)
-        lblBall3.Top = CInt(Rnd() * frmMoving.ActiveForm.Height)
+        lblBall3.Left = CInt(Rnd() * Form1.ActiveForm.Width)
+        lblBall3.Top = CInt(Rnd() * Form1.ActiveForm.Height)
     End Sub
